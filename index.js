a66d3774da97258b88d943d9d037935d208e00d9
vampire <aryangupta98711@gmail.com>
2024-01-04 22:26:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-let ar=require("./second");
-console.log("hrh");
-console.log(ar);
+import express from "express";
+const a=express();
+a.listen(7000,()=>{
+    console.log("the port is running ");
+})
+// to find the port number which is running on our computer
+// type command netstat -ano|findstr "LISTENING"
