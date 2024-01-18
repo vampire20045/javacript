d0937c32cc847becded78b06f16bd1a4d1d243a3
vampire <aryangupta98711@gmail.com>
2024-01-18 15:44:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-import express from "express";
-const a=express();
-a.listen(7000,()=>{
-    console.log("the port is running ");
-})
-// to find the port number which is running on our computer
-// type command netstat -ano|findstr "LISTENING"
+import express from 'express';
+import bodyparser from 'body-parser';
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.post("/submit", (req, res) => {
+  const n = req.body['first'];
+  const a = req.body['second'];
+  const b = parseInt(n) + parseInt(a);
+  
+  res.render("index.ejs", { answer: b });
+});
+
+app.listen(2000, (err) => {
+  if (err) {
+    console.error("Error starting the server:", err);
+  } else {
+    console.log("Server is ready on port 2000");
+  }
+});
